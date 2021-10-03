1a3c240d7618297abd54accd42a00dde1b25a9d0
BrayanMuri <84592402+BrayanMuri@users.noreply.github.com>
2021-10-02 19:13:02 -0500

    componetes header

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,12 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Componente{
+  icon: string;
+  name: string;
+  redirecTo: string;
+}
+
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-
+  
+  componetes: any[] = [
+    {
+      icon: 'airplane-sharp',
+      name: 'Action Sheet',
+      redirecTo: '/action-sheet'
+    },
+    {
+      icon: 'alarma-sharp',
+      name: 'Alert Sheet',
+      redirecTo: '/alert'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
